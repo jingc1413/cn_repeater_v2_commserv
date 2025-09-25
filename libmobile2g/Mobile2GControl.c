bede90eee06af903550da710ea7690f7b2cf2850
jingc <406898096@qq.com>
2025-09-25 17:03:27 +0800

    add 0xB2 工厂参数查询，打包

--- libmobile2g/Mobile2GControl.c
+++ libmobile2g/Mobile2GControl.c
@@ -151,9 +151,9 @@ int Encode_Das(int CommType, int CommandType, int NetFlag, REPEATERINFO*  stuRep
 	{
 		nRet=EncodeDasAppLayer(SETCOMMAND,0xFF,ObjList,objSize,pPack);
 	}
-	/*else if(CommandType == 0xB2){
+	else if(CommandType == 0xB2){
 		nRet=EncodeDasAppLayer(0xB2,0xFF,ObjList,objSize,pPack);
-	}*/
+	}
 	//增加工厂模式2008.1.9 begin
 	else if(CommandType==FCTPRMQRY)//工厂参数查询
 	{
